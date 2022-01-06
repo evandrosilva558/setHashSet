0f3168a8299a7b30f9f76ee00ca885fb5d40defc
Evandro <evandrosilva558@hotmail.com>
2022-01-06 07:02:24 -0300

    criado a classe e objeto na lista nao ordenada porem mapeada com o metodo HashSet

--- src/dio/setHashSet/Exec.java
+++ src/dio/setHashSet/Exec.java
@@ -1,9 +1,22 @@
 package dio.setHashSet;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Exec {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// criando uma lista aleatoria adicionando objetos com HashSet.
+		Set<Carro> hashSetCarros = new HashSet<>();
+		
+		hashSetCarros.add(new Carro("Fiat"));
+		hashSetCarros.add(new Carro("Ford"));
+		hashSetCarros.add(new Carro("Alfa Romeo"));
+		hashSetCarros.add(new Carro("Chevrolet"));
+		
+		// imprime os objetos.
+		System.out.println(hashSetCarros);
 
 	}
 
